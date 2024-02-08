c1f374fb9321eb1367a8f7587eb4796203f2c527
BohdanBykov <bv.bykov@gmail.com>
2024-02-08 15:27:06 +0100

    feat: Update configs

--- Strapi/config/server.ts
+++ Strapi/config/server.ts
@@ -1,6 +1,6 @@
 export default ({ env }) => ({
   host: env('HOST', '0.0.0.0'),
-  url: env('PUBLIC_URL', 'http://172.233.60.71:1337'),
+  url: env('PUBLIC_URL', 'localhost'),
   port: env.int('PORT', 1337),
   app: {
     keys: env.array('APP_KEYS'),
